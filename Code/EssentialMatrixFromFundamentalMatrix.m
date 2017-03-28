07f7c7cd292a023ec6e550913cabd6271da561e9
Wei-An Lin <andylin@ramawks78.umiacs.umd.edu>
2017-03-28 10:20:53 -0400

    EssentialMatrixFromFundamentalMatrx.m and add dispMatchedFeatures.m for testing

--- Code/EssentialMatrixFromFundamentalMatrix.m
+++ Code/EssentialMatrixFromFundamentalMatrix.m
@@ -6,5 +6,9 @@ function E = EssentialMatrixFromFundamentalMatrix(F,K)
 % E: 3x3 essential matrix with singular values (1,1,0)
 
 %% Your Code goes here
+E = K' * F * K;
+[U, D, V] = svd(E);
+D(1:4:end) = [1 1 0];
+E = U * D * V';
 
 end
