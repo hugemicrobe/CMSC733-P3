1f03a9b8b1058b9ee0e935fc7364d06a93a6afd6
huaijenliang <r81810g@gmail.com>
2017-03-29 11:47:10 -0400

    implement nonlinearTri

--- Code/NonlinearTriangulation.m
+++ Code/NonlinearTriangulation.m
@@ -8,6 +8,39 @@ function X = NonlinearTriangulation(K, C1, R1, C2, R2, x1, x2, X0)
 % (INPUT and OUTPUT) X: Nx3 matrix whose row represents 3D triangulated point.
 
 %% Your code goes here
+params0 = encodeParameters(X0);
+opts = optimoptions(@lsqnonlin, 'Algorithm', 'levenberg-marquardt', 'TolX', 1e-64, 'TolFun', 1e-64, 'MaxFunEvals', 1e64, 'MaxIter', 1e64, 'Display', 'iter');
+paramsFinal = lsqnonlin(@(params)geoError(K, C1, R1, C2, R2, x1, x2, params), params0, [], [], opts);
+X = decodeParameters(paramsFinal);
 
 end
 
+function params = encodeParameters(X0)
+    params = reshape(X0, [], 1);
+end
+
+function X = decodeParameters(params)
+    X = reshape(params, [], 3);
+end
+
+function f = geoError(K, C1, R1, C2, R2, x1, x2, params)
+    X = decodeParameters(params);
+    assert(size(x1, 1) == size(X, 1), 'The number of matching points in the first image are not the same');
+    assert(size(x2, 1) == size(X, 1), 'The number of matching points in the second are not the same');
+    
+    % Do the projection and calculate geo error
+    N = size(X, 1);
+    f = zeros(N, 2, 2);
+    P1 = K * [R1, -R1*C1];
+    P2 = K * [R2, -R2*C2];
+    homX = [X; ones(N, 1)]; % N x 3
+    pX1 = P1 * homX'; % 3 x N
+    pX2 = P2 * homX'; % 3 x N
+    pX1 = pX1'; % N x 3
+    pX2 = pX2'; % N x 3
+    pX1 = pX1 ./ repmat(pX1(:, 3), 3, 1); % normalize
+    pX2 = pX2 ./ repmat(pX1(:, 3), 3, 1); % normalize
+    % error
+    f(:, :, 1) = x1 - pX1(:, 1:2);
+    f(:, :, 2) = x2 - pX2(:, 1:2);
+end
