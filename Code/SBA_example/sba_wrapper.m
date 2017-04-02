f610662671fb4facd0036020c212574c33ecd33a
huaijenliang <r81810g@gmail.com>
2017-04-02 14:21:18 -0400

    add sba

--- Code/SBA_example/sba_wrapper.m
+++ Code/SBA_example/sba_wrapper.m
@@ -12,11 +12,13 @@ r0 = [];    cams = [];  pts2D = [];
 spmask=sparse([], [], [], 1, nFrames);
 
 % Concaternate all camera poses into cams vector
-for i = 1 : nFrames   
-    q = Matrix2Quaternion(inv(K)*cP{i}(1:3,1:3));
-    q = QuaternionNormalization(q);
+for i = 1 : nFrames
+    P = K \ cP{i};
+    % q = Matrix2Quaternion(inv(K)*cP{i}(1:3,1:3));
+    % q = QuaternionNormalization(q);
+    q = R2q(P(1:3, 1:3));
     r0 = [r0;q'];
-    cams = [cams;q' GetCameraPosition(cP{i}, K)'];
+    cams = [cams;q' P(:, 4)'];
 end
 
 % Concaternate all 2D measurements into pts2D
@@ -41,7 +43,7 @@ r0=reshape(r0', 1, numel(r0));
 cal = [K(1,1) K(1,2), K(1,3), K(2,2), K(2,3)];
 
 opts=[1E-1, 0, 0, 1E-5, 0.0];
-p0=[Mat2Vec(cams)' Mat2Vec(pts3D)'];
+p0=[reshape(cams', 1, []) reshape(pts3D', 1, [])];
 
 if isreal(p0) ~= 1
     k = 1;
@@ -51,9 +53,10 @@ end
 % Retrieve paramters
 for i = 1 : nFrames
     camera = p(7*(i-1)+1:7*i)';
-    R = Quaternion2Matrix(camera(1:4));
+    R = q2R(camera(1:4));
     C = camera(5:end);
-    cP{i} = K*R*[eye(3), -C];
+    % cP{i} = K*R*[eye(3), -C];
+    cP{i} = K * [R, C];
 end
 
 X = [];
